f042f2c1802be0fbacf767165acc03f626bfce63
Funny-Bunny <dragos281993@gmail.com>
2017-09-14 21:14:25 +0300

    Inherit gapps
    Signed-off-by: Funny-Bunny <dragos281993@gmail.com>

--- mk_files/main.mk
+++ mk_files/main.mk
@@ -48,3 +48,7 @@ PRODUCT_PACKAGES += \
 
 # Boot animation
 $(call inherit-product-if-exists, vendor/vanilla/mk_files/bootanimation.mk)
+
+# Inherit gapps
+$(call inherit-product-if-exists, vendor/gapps/gapps.mk)
+
