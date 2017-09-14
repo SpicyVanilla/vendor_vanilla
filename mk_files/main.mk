0c4cf499f274a93d4e1101bcc5f4af29f4f37af9
Funny-Bunny <dragos281993@gmail.com>
2017-09-14 20:14:17 +0300

    Add essential things
    Signed-off-by: Funny-Bunny <dragos281993@gmail.com>

---
+++ mk_files/main.mk
@@ -0,0 +1,25 @@
+PRODUCT_PROPERTY_OVERRIDES += \
+    keyguard.no_require_sim=true \
+    ro.control_privapp_permissions=enforce \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+    ro.com.google.clientidbase=android-google \
+    ro.error.receiver.system.apps=com.google.android.gms \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.com.android.dataroaming=false \
+    ro.atrace.core.services=com.google.android.gms,com.google.android.gms.ui,com.google.android.gms.persistent \
+    net.tethering.noprovisioning=true \
+    persist.sys.dun.override=0 \
+    ro.build.selinux=1 \
+    ro.adb.secure=0 \
+    ro.setupwizard.rotation_locked=true \
+    ro.opa.eligible_device=true
+
+# Common overlay
+PRODUCT_PACKAGE_OVERLAYS += \
+    vendor/vanilla/overlay
+
+# Extra packages
+PRODUCT_PACKAGES += \
+    Stk
+
